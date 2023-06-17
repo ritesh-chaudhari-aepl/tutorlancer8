59a1b40e1bc1dd4534695bc2129646cc6d8b6325
akriti <akritisweta1999@gmail.com>
2023-06-17 19:34:12 +0530

    ux8 fully  done

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
-
-// const withImages = require('next-images')
-// module.exports = withImages()
--- src/common/Divider.jsx
+++
@@ -1,9 +0,0 @@
-const Divider = () => {
-    return <span className=' h-1 w-20  sm:h-14 sm:w-1 bg-white' />
-  }
-  
-  export const VerticalDivider = () => {
-    return <span className=' h-10 w-1 bg-white' />
-  }
-  
-  export default Divider
